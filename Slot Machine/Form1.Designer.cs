b9579ad2556c60d9a402ab12a9d9258d04106f08
Sabrina Turney <turney.sabrina@gmail.com>
2020-01-03 21:23:23 -0500

    Add files via upload

---
+++ Slot Machine/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Slot_Machine
+{
+    partial class slotMachine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(slotMachine));
+            this.firstPictureBox = new System.Windows.Forms.PictureBox();
+            this.secondPictureBox = new System.Windows.Forms.PictureBox();
+            this.thirdPictureBox = new System.Windows.Forms.PictureBox();
+            this.amountLabel = new System.Windows.Forms.Label();
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.spinButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.fruitList = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.firstPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thirdPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // firstPictureBox
+            // 
+            this.firstPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.firstPictureBox.Location = new System.Drawing.Point(12, 24);
+            this.firstPictureBox.Name = "firstPictureBox";
+            this.firstPictureBox.Size = new System.Drawing.Size(168, 161);
+            this.firstPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.firstPictureBox.TabIndex = 0;
+            this.firstPictureBox.TabStop = false;
+            // 
+            // secondPictureBox
+            // 
+            this.secondPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.secondPictureBox.Location = new System.Drawing.Point(208, 24);
+            this.secondPictureBox.Name = "secondPictureBox";
+            this.secondPictureBox.Size = new System.Drawing.Size(168, 161);
+            this.secondPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.secondPictureBox.TabIndex = 1;
+            this.secondPictureBox.TabStop = false;
+            // 
+            // thirdPictureBox
+            // 
+            this.thirdPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.thirdPictureBox.Location = new System.Drawing.Point(393, 24);
+            this.thirdPictureBox.Name = "thirdPictureBox";
+            this.thirdPictureBox.Size = new System.Drawing.Size(168, 161);
+            this.thirdPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.thirdPictureBox.TabIndex = 2;
+            this.thirdPictureBox.TabStop = false;
+            // 
+            // amountLabel
+            // 
+            this.amountLabel.AutoSize = true;
+            this.amountLabel.Location = new System.Drawing.Point(73, 231);
+            this.amountLabel.Name = "amountLabel";
+            this.amountLabel.Size = new System.Drawing.Size(255, 17);
+            this.amountLabel.TabIndex = 3;
+            this.amountLabel.Text = "Enter amount to gamble on the slots!  $";
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(346, 228);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(100, 22);
+            this.amountTextBox.TabIndex = 4;
+            // 
+            // spinButton
+            // 
+            this.spinButton.Location = new System.Drawing.Point(138, 265);
+            this.spinButton.Name = "spinButton";
+            this.spinButton.Size = new System.Drawing.Size(120, 53);
+            this.spinButton.TabIndex = 5;
+            this.spinButton.Text = "Spin the Slots!";
+            this.spinButton.UseVisualStyleBackColor = true;
+            this.spinButton.Click += new System.EventHandler(this.SpinButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(291, 265);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(120, 53);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // fruitList
+            // 
+            this.fruitList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("fruitList.ImageStream")));
+            this.fruitList.TransparentColor = System.Drawing.Color.Transparent;
+            this.fruitList.Images.SetKeyName(0, "Apple.bmp");
+            this.fruitList.Images.SetKeyName(1, "Banana.bmp");
+            this.fruitList.Images.SetKeyName(2, "Cherries.bmp");
+            this.fruitList.Images.SetKeyName(3, "Grapes.bmp");
+            this.fruitList.Images.SetKeyName(4, "Lemon.bmp");
+            this.fruitList.Images.SetKeyName(5, "Lime.bmp");
+            this.fruitList.Images.SetKeyName(6, "Orange.bmp");
+            this.fruitList.Images.SetKeyName(7, "Pear.bmp");
+            this.fruitList.Images.SetKeyName(8, "Strawberry.bmp");
+            this.fruitList.Images.SetKeyName(9, "Watermelon.bmp");
+            // 
+            // slotMachine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 330);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.spinButton);
+            this.Controls.Add(this.amountTextBox);
+            this.Controls.Add(this.amountLabel);
+            this.Controls.Add(this.thirdPictureBox);
+            this.Controls.Add(this.secondPictureBox);
+            this.Controls.Add(this.firstPictureBox);
+            this.Name = "slotMachine";
+            this.Text = "Slot Machine";
+            ((System.ComponentModel.ISupportInitialize)(this.firstPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.thirdPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox firstPictureBox;
+        private System.Windows.Forms.PictureBox secondPictureBox;
+        private System.Windows.Forms.PictureBox thirdPictureBox;
+        private System.Windows.Forms.Label amountLabel;
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.Button spinButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ImageList fruitList;
+    }
+}
+
---
+++ Slot Machine/Form1.cs
@@ -0,0 +1,106 @@
+﻿//Sabrina Turney
+//C# I
+//Assignment 9-3: Slot Machine Simulation
+//Last edited: 09/28/2019
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Slot_Machine
+{
+    public partial class slotMachine : Form
+    {
+        public slotMachine()
+        {
+            InitializeComponent();
+        }
+
+        //First we declare all our variables
+        //We have to get the numerical bet entered,
+        //as well as compound wins if there are any.
+        //Our spins must also be random or the user
+        //would win every round!
+        decimal amtEntered;
+        Random rand = new Random();
+        decimal total;
+        decimal firstWin;
+        decimal secondWin;
+        decimal totalWins;
+
+        //Here is our function to actually spin the "slot machine".
+        private void slotSpin()
+        {
+            //We start out with a try block, just in case there are issues.
+            try
+            {
+                //Find the amount in the betting box:
+                amountTextBox.Focus();
+                int index1 = rand.Next(fruitList.Images.Count);
+                //Set our fruits to whatever index value they hold in the image list.
+                int index2 = new int();
+                index2 = rand.Next(fruitList.Images.Count);
+                //This way, we know if the images match.
+                int index3 = new int();
+                index3 = rand.Next(fruitList.Images.Count);
+
+                //Here is where each index is set to the actual viewing box for the viewer.
+                firstPictureBox.Image = fruitList.Images[index1];
+                secondPictureBox.Image = fruitList.Images[index2];
+                thirdPictureBox.Image = fruitList.Images[index3];
+
+                //Take the bet and make it a reasonable "dollar" readable amount.
+                amtEntered = decimal.Parse(amountTextBox.Text.Trim());
+                total += amtEntered;
+
+                //If the user wins by two slots or three slots, we compound their winnings.
+                firstWin = amtEntered * 2;
+                secondWin = amtEntered * 3;
+
+                //But obviously, we have to start their wins off at zero.
+                totalWins = 0;
+
+
+                //If statement here: If two of the slots are correct (index values must be equal)!
+                if (index1 == index2 || index1 == index3 || index2 == index3)
+                {
+                    //We congratulate the user on their win, and let them know how much they got!
+                    MessageBox.Show("Congratulations! Two slots! You have won: " + firstWin.ToString("c") + " !");
+                }
+                //Or if they get all three values correct, they get even more money!
+                else if (index1 == index2 && index1 == index3 && index2 == index3)
+                {
+                    MessageBox.Show("Amazing! Three slots! Congratulations! You have won: " + secondWin.ToString("c") + " !");
+                }
+                else
+                //Yikes, no win here. We have to give some hard news to our gamblers.
+                {
+                    MessageBox.Show("No luck this time! Try again!");
+                }
+            }
+            catch (Exception ex)
+            {
+                //Obligatory catch if something doesn't work, so we can see what the issue is.
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //Close the program - Easy!
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //Here is the actual button click for the program
+        //in turn, it runs the giant function up above :)
+        private void SpinButton_Click(object sender, EventArgs e)
+        {
+            slotSpin();
+        }
+    }
+}
